a6727733fd06c7370bdb0e1a8a785770436262e2
paulwinex <paulwinex@gmail.com>
2025-08-17 21:16:43 +0300

    rename service

--- agio_publish_simple/plugins/services/launcher_service.py
+++ agio_publish_simple/plugins/services/launcher_service.py
@@ -7,13 +7,13 @@ logger = logging.getLogger(__name__)
 
 
 class SimpleLauncherService(ServicePlugin):
-    name = 'simple_launcher'
+    name = 'simple_publish'
 
     def execute(self, **kwargs):
         pass
 
     @make_action(menu_name='task.launcher', app_name='front')
-    def start_publisher(self, *args, task_id: str, **kwargs):
+    def open_publisher_dialog(self, *args, task_id: str, **kwargs):
         logger.info(f'Start standalone publisher with task {task_id}')
         workspace_id = None # ATask(task_id).project.workspace_id
         args = [
