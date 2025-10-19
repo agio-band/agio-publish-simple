f6b9714683c9a617d80c75db0a24d6aa7ce21dfd
paulwinex <paulwinex@gmail.com>
2025-10-19 16:15:14 +0300

    update PublishEngineSimplePlugin

--- agio_publish_simple/plugins/publish/simple_publish_engine.py
+++ agio_publish_simple/plugins/publish/simple_publish_engine.py
@@ -16,19 +16,25 @@ class PublishEngineSimplePlugin(PublishEngineBasePlugin):
         selected_instances = options.get('selected_instances')
         if selected_instances:
             logger.info('Selected instances: {}'.format(selected_instances))
+        instances = self.instances.values()
+        if selected_instances:
+            instances = [inst for inst in instances if inst.name in selected_instances]
+        emit('pipe.publish.before_publish_processing', {'instances': instances})
         for inst_id, inst in self.instances.items():
             inst: PublishInstance
-            if selected_instances:
-                if inst.name not in selected_instances:
-                    logger.info('Skip instance %s', inst.name)
-                    continue
+            emit('pipe.publish.before_instance_processing', {'instances': inst})
+            if not inst.enabled:
+                continue
             publisher_cls = get_publisher(inst.product.type)
             publisher = publisher_cls(inst, options)
             published_files = publisher.publish(**options)
             if not published_files:
                 raise Exception(f'No published files created with instance {inst}')
-            publish_result.append({
+            instance_result = {
                 'instance': inst,
                 'published_files': published_files
-            })
+            }
+            emit('pipe.publish.instance_processed', instance_result)
+            publish_result.append(instance_result)
+        emit('pipe.publish.after_publish_processing', {'result': publish_result})
         return publish_result
