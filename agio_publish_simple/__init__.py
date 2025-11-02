5db06f7fe6a0cee175eabe39c8bd6f3ca647e771
paulwinex <paulwinex@gmail.com>
2025-11-02 15:53:22 +0300

    fix init script

--- agio_publish_simple/__init__.py
+++ agio_publish_simple/__init__.py
@@ -2,4 +2,4 @@ import yaml
 from pathlib import Path
 
 __all__ = ['__version__']
-__version__ = yaml.safe_load(Path(__file__, '../__agio__.yml').resolve().read_text(encoding='utf-8'))['version']
+__version__ = yaml.safe_load(Path(__file__, '../__agio__.yml').resolve().read_text(encoding='utf-8')).get('version', '0.0.0')
